d1f3e58c6acc51f6201e67adaa8ab0925a006486
Wessel van der Veen <wvn@synadia.nl>
2019-09-18 13:52:49 +0200

    Fixes outputs

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,5 @@
-output "target_group_name" {
-  description = "ALB Target group name"
-  value       = data.aws_lb_target_group.default.name
-}
-
 output "target_group_arn" {
   description = "ALB Target group ARN"
-  value       = data.aws_lb_target_group.default.arn
-}
-
-output "target_group_arn_suffix" {
-  description = "ALB Target group ARN suffix"
-  value       = data.aws_lb_target_group.default.arn_suffix
+  value       = local.target_group_arn
 }
 
